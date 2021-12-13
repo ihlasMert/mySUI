cac78d3dae73b5842824bc76542a4a10afac6113
İhlas Mert <ihlas1mert@gmail.com>
2021-12-13 13:41:29 +0300

    My First SwiftUI

---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by ihlas on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
